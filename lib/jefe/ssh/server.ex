afda6bb348f266b421ea7b633a8103828d9706eb
Graeme Coupar <grambo@grambo.me.uk>
2016-11-21 23:11:06 +0000

    SSH.Server will generate it's own host keys.
    If there are no host keys on startup, then SSH.Server will now generate
    them.

--- lib/jefe/ssh/server.ex
+++ lib/jefe/ssh/server.ex
@@ -13,13 +13,17 @@ defmodule Jefe.SSH.Server do
   end
 
   def init(nil) do
+    File.mkdir_p!(system_dir)
+    gen_key("ssh_host_rsa_key", "rsa")
+    gen_key("ssh_host_dsa_key", "dsa")
+
     {:ok, pid} = :ssh.daemon(
       {0,0,0,0},
       5867,
       shell: &spawn_session/1,
       # TODO: Ideally we should be able to generate our own ssh keys
       # on startup if there aren't already some in place...
-      system_dir: '/Users/graeme/src/securi/test/test_ssh_keys',
+      system_dir: system_dir |> String.to_charlist,
       auth_methods: 'publickey'
     )
     true = Process.link(pid)
@@ -33,5 +37,21 @@ defmodule Jefe.SSH.Server do
       |> ClientSession.start_link
 
     pid
+  end
+
+  # The path to our SSH servers system directory.
+  defp system_dir do
+    Path.expand("~/.jefe/ssh_server/")
+  end
+
+  # Generates an SSH key of the provided type in system_dir
+  defp gen_key(filename, type) do
+    unless File.exists?(Path.join(system_dir, filename)) do
+      {_output, 0} = System.cmd(
+        "ssh-keygen",
+        ["-f", filename, "-N", "", "-t", type],
+        cd: system_dir
+      )
+    end
   end
 end
