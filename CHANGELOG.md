b374ea2776229db9b45ddfa5aa06d6e4479bfb21
Graeme Coupar <grambo@grambo.me.uk>
2017-02-20 14:48:01 +0000

    Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+### v0.1.0 (20/02/16)
+
+- Initial release.
+- Supports Procfiles & dotenv.
+- Provides ssh access to running processes.
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jefe.Mixfile do
 
   def project do
     [app: :jefe,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
